07597c8f4e735026a55de5ff6185876e8607daa0
Graeme Holliday <graeme.holliday@pm.me>
2023-06-13 03:47:24 +0000

    add data for fcfs, random, sjf

--- test.jl
+++ test.jl
@@ -5,8 +5,13 @@ using Flux.Losses
 include("./cluster.jl")
 using .cluster
 
-for i in 1:21
+# repeat for random, fcfs, sjf
+for i in 1:19
     env = ClusterEnv(i)
-    run(RandomPolicy(), env, StopAfterEpisode(1))
-    println("Env $i FCFS metrics: ", env.metrics)
+    hook = DoEveryNStep(;n=100000) do t, agent, env
+        println("Job ", env.next_job_index, "/", length(env.workload.jobs))
+    end
+    stop = StopAfterEpisode(1)
+    run(RandomPolicy(), env, stop, hook)
+    println("Metrics $i: ", env.metrics)
 end
